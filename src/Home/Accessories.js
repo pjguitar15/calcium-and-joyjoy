70222e478c4429a9c5c9c6b3fb2bb76a9aa25a24
jasonLapina <lapina.jason@gmail.com>
2023-10-20 18:55:39 +0800

    accessories responsiveness

--- src/Home/Accessories.js
+++ src/Home/Accessories.js
@@ -3,14 +3,60 @@ import { ChevronRightIcon, ChevronLeftIcon } from "@chakra-ui/icons";
 import ItemCard from "../Shared/ItemCard";
 
 import Slider from "react-slick";
+import { useState } from "react";
 
 function Accessories() {
+  const [activeIndex, setActiveIndex] = useState(0);
   const settings = {
     dots: false,
     infinite: false,
     speed: 500,
     slidesToShow: 5,
     slidesToScroll: 5,
+    nextArrow: (
+      <Arrow variant='next' onLast={activeIndex === 5 ? true : false} />
+    ),
+    prevArrow: (
+      <Arrow variant='prev' onLast={activeIndex === 0 ? true : false} />
+    ),
+    beforeChange: (current, next) => setActiveIndex(next),
+    responsive: [
+      {
+        breakpoint: 1500,
+        settings: {
+          slidesToShow: 4,
+          slidesToScroll: 4,
+        },
+      },
+      {
+        breakpoint: 1230,
+        settings: {
+          slidesToShow: 3,
+          slidesToScroll: 3,
+        },
+      },
+      {
+        breakpoint: 940,
+        settings: {
+          slidesToShow: 2,
+          slidesToScroll: 2,
+        },
+      },
+      {
+        breakpoint: 940,
+        settings: {
+          slidesToShow: 2,
+          slidesToScroll: 2,
+        },
+      },
+      {
+        breakpoint: 650,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1,
+        },
+      },
+    ],
   };
   const dummy = Array.from({ length: 5 });
   return (
--- src/Home/ShoeList.js
+++ src/Home/ShoeList.js
@@ -19,6 +19,43 @@ function ShoeList() {
       <Arrow variant='prev' onLast={activeIndex === 0 ? true : false} />
     ),
     beforeChange: (current, next) => setActiveIndex(next),
+    responsive: [
+      {
+        breakpoint: 1500,
+        settings: {
+          slidesToShow: 4,
+          slidesToScroll: 4,
+        },
+      },
+      {
+        breakpoint: 1230,
+        settings: {
+          slidesToShow: 3,
+          slidesToScroll: 3,
+        },
+      },
+      {
+        breakpoint: 940,
+        settings: {
+          slidesToShow: 2,
+          slidesToScroll: 2,
+        },
+      },
+      {
+        breakpoint: 940,
+        settings: {
+          slidesToShow: 2,
+          slidesToScroll: 2,
+        },
+      },
+      {
+        breakpoint: 650,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1,
+        },
+      },
+    ],
   };
 
   return (
