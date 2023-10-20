fabac39d4e0cd7218941e758bbb35c3a5ce5de1a
jasonLapina <lapina.jason@gmail.com>
2023-10-20 19:25:31 +0800

    add footer

--- src/Home/Homepage.js
+++ src/Home/Homepage.js
@@ -8,17 +8,18 @@ function Homepage() {
         p='48px'
         borderRadius='24px'
         bgImage='linear-gradient(to bottom right, rgba(0,0,0,.9) ,rgba(0,0,0,.6))'
-        gridTemplateColumns='1fr 1fr'
+        gridTemplateColumns={{ lg: "1fr 1fr", base: "1fr" }}
       >
         <Text
           color='white'
           filter='drop-shadow(2px 2px 2px red)'
-          fontSize='120px'
+          fontSize={{ base: "80px", lg: "120px" }}
           fontWeight='bold'
+          textAlign={{ base: "center", lg: "left" }}
         >
           SAMPLE <br /> PROTOTYPE
         </Text>
-        <Skeleton w='100%' />
+        <Skeleton w='100%' h='400px' />
       </Grid>
       <ShoeList />
       <Accessories />
--- src/Shared/Layout.js
+++ src/Shared/Layout.js
@@ -1,14 +1,52 @@
-import { Box } from "@chakra-ui/react";
+import { Box, HStack, Icon, Text } from "@chakra-ui/react";
 import { Outlet } from "react-router-dom";
 import Navbar from "./Navbar";
+
+import { BiLogoTwitter, BiLogoFacebook, BiLogoInstagram } from "react-icons/bi";
+import { FaLocationDot } from "react-icons/fa6";
 function Layout() {
+  const socials = [
+    {
+      icon: BiLogoFacebook,
+      link: "https://www.facebook.com/",
+    },
+    {
+      icon: BiLogoTwitter,
+      link: "https://twitter.com/home",
+    },
+    {
+      icon: BiLogoInstagram,
+      link: "https://www.facebook.com/",
+    },
+  ];
   return (
     <>
       <Navbar />
       <Box maxW='var(--maxW)' mx='auto' p='40px'>
         <Outlet />
       </Box>
-      <Box h='30vh' bgColor='gray.600' mt='80px' py='40px' px='16px'></Box>
+      <Box h='240px' bgColor='#403F3F' mt='80px' pb='56px' pt='200px' px='40px'>
+        <HStack justifyContent='space-between'>
+          <HStack gap='16px' color='#FFDC83'>
+            <Text fontWeight='bold'>
+              <Icon as={FaLocationDot} /> Philippines
+            </Text>
+            <Text opacity={0.6}>&copy; 2023 Calcium & Joyjoy</Text>
+          </HStack>
+          <HStack fontSize='32px' gap='16px'>
+            {socials.map((item) => (
+              <Icon
+                cursor='pointer'
+                bgColor='#FFDC83'
+                key={item.link}
+                as={item.icon}
+                borderRadius='full'
+                p='6px'
+              />
+            ))}
+          </HStack>
+        </HStack>
+      </Box>
     </>
   );
 }
