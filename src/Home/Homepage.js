84d231283a1cab48cb554e9578da60e05e7ea216
jasonLapina <lapina.jason@gmail.com>
2023-10-22 00:06:27 +0800

    hero section style change

--- src/Home/Homepage.js
+++ src/Home/Homepage.js
@@ -1,51 +1,36 @@
-import {
-  Box,
-  Center,
-  Grid,
-  Image,
-  Skeleton,
-  Text,
-  VStack,
-} from "@chakra-ui/react";
+import { Box, Grid, Skeleton, Text, VStack } from "@chakra-ui/react";
 import ShoeList from "./ShoeList";
 import Accessories from "./Accessories";
 function Homepage() {
   return (
     <VStack align='normal' justify='normal' gap='80px'>
       <Grid
-        p='48px'
         borderRadius='24px'
         bgImage='linear-gradient(to bottom right, rgba(0,0,0,.9) ,rgba(0,0,0,.6))'
-        gridTemplateColumns={{ lg: "6fr 4fr", base: "1fr" }}
-        h='70vh'
+        h='80vh'
+        pos='relative'
+        gridTemplateColumns={{ base: "1fr", lg: "1fr 1fr" }}
       >
         <Text
-          color='white'
-          filter='drop-shadow(2px 2px 2px red)'
-          fontSize={{ base: "80px", lg: "140px" }}
           fontWeight='bold'
-          textAlign={{ base: "center", lg: "left" }}
-          lineHeight='140px'
-          pt='80px'
+          filter='drop-shadow(4px 4px 2px #FF0707)'
+          fontSize='8vw'
+          color='white'
+          lineHeight='shorter'
           pl='40px'
+          pt='56px'
+          letterSpacing='tight'
         >
-          SAMPLE <br /> PROTOTYPE
+          SAMPLE
+          <br />
+          PROTOTYPE
         </Text>
-
-        <Box pos='relative'>
-          <Image
-            right='-120px'
-            top='80px'
+        <Box display='flex' pos='relative' alignItems='center'>
+          <Skeleton
+            w='100%'
+            h='400px'
             pos='absolute'
-            src='/heroSocks.png'
-            filter='brightness(.7)'
-          />
-          <Image
-            pos='absolute'
-            src='/airJordan.png'
-            transform='rotate(-24deg)'
-            left='-200px'
-            bottom='0'
+            // left={{ lg: "-3vw" }}
           />
         </Box>
       </Grid>
