dc1ca5adeed4b812218b4936e33a6fe9dc27ec73
jasonLapina <lapina.jason@gmail.com>
2023-10-25 04:35:01 +0800

    add checkout payment page

--- src/Checkout/OrderSummary.js
+++ src/Checkout/OrderSummary.js
@@ -1,4 +1,13 @@
-import { HStack, Text, Box, Heading } from "@chakra-ui/react";
+import {
+  HStack,
+  Text,
+  Box,
+  Heading,
+  Grid,
+  Image,
+  VStack,
+  Divider,
+} from "@chakra-ui/react";
 function OrderSummary() {
   return (
     <Box>
@@ -24,6 +33,23 @@ function OrderSummary() {
         <Text>Total</Text>
         <Text>&#8369;1</Text>
       </HStack>
+
+      <Grid gap='16px' alignItems='center' gridTemplateColumns='1fr 1fr'>
+        <Image w='200px' h='200px' src='dummyShoe.png' loading='lazy' />
+        <VStack
+          fontWeight='semibold'
+          fontSize='14px'
+          gap='0px'
+          alignItems='normal'
+        >
+          <Text fontWeight='bold'>Air Force 1 White</Text>
+          <Text color='gray.500'>Men/Women's Shoes</Text>
+          <Text>Size: 9</Text>
+          <Text>Quantity: 1</Text>
+          <Text>&#8369;1</Text>
+        </VStack>
+      </Grid>
+      <Divider mt='-32px' />
     </Box>
   );
 }
