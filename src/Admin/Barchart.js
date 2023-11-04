6b3eb2d11cf19f0093edf1bbe7617b2c6502c434
jasonLapina <lapina.jason@gmail.com>
2023-11-05 02:33:05 +0800

    add pie chart

--- src/Admin/Barchart.js
+++ src/Admin/Barchart.js
@@ -3,10 +3,10 @@ import { Bar } from "react-chartjs-2";
 import { Chart as ChartJS } from "chart.js/auto";
 
 const dummy = [
-  { day: "Monday", sales: 20 },
-  { day: "Tuesday", sales: 35 },
-  { day: "Wednesday", sales: 10 },
-  { day: "Thursday", sales: 20 },
+  { day: "2d ago", sales: 20 },
+  { day: "1d ago", sales: 35 },
+  { day: "Yesterday", sales: 10 },
+  { day: "Today", sales: 20 },
 ];
 
 function Barchart() {
---
+++ src/Admin/PieChart.js
@@ -0,0 +1,6 @@
+import { Box } from "@chakra-ui/react";
+function PieChart() {
+  return <Box></Box>;
+}
+
+export default PieChart;
