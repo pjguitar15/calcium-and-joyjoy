ecf602ce527abb70fc82b815e023f6d380302dba
jasonLapina <lapina.jason@gmail.com>
2023-11-05 04:17:54 +0800

    Add piechart

--- src/Admin/Overview.js
+++ src/Admin/Overview.js
@@ -2,6 +2,7 @@ import { Box, Grid, HStack, Heading, Icon, Text } from "@chakra-ui/react";
 import ViewLayout from "./ViewLayout";
 import { AiOutlineStock } from "react-icons/ai";
 import Barchart from "./Barchart";
+import { PieChart } from "./PieChart";
 function Overview() {
   const dummy = Array.from({ length: 3 });
 
@@ -38,9 +39,15 @@ function Overview() {
           </Box>
         ))}
       </Grid>
-      <Grid mt='40px' gridTemplateColumns='1fr 1fr' gap='24px'>
+      <Grid
+        mt='40px'
+        gridTemplateColumns='1fr 1fr'
+        justifyContent='center'
+        justifyItems='center'
+        gap='24px'
+      >
         <Barchart />
-        <Barchart />
+        <PieChart />
       </Grid>
     </ViewLayout>
   );
--- src/Admin/PieChart.js
+++ src/Admin/PieChart.js
@@ -1,6 +1,36 @@
+import React from "react";
+import { Chart as ChartJS, ArcElement, Tooltip, Legend } from "chart.js";
+import { Pie } from "react-chartjs-2";
 import { Box } from "@chakra-ui/react";
-function PieChart() {
-  return <Box></Box>;
-}
 
-export default PieChart;
+ChartJS.register(ArcElement, Tooltip, Legend);
+
+export const data = {
+  labels: ["Red", "Blue", "Yellow", "Green"],
+  datasets: [
+    {
+      label: "# of Votes",
+      data: [12, 19, 3, 5],
+      backgroundColor: [
+        "rgba(255, 99, 132, 0.2)",
+        "rgba(54, 162, 235, 0.2)",
+        "rgba(255, 206, 86, 0.2)",
+        "rgba(75, 192, 192, 0.2)",
+      ],
+      borderColor: [
+        "rgba(255, 99, 132, 1)",
+        "rgba(54, 162, 235, 1)",
+        "rgba(255, 206, 86, 1)",
+        "rgba(75, 192, 192, 1)",
+      ],
+      borderWidth: 1,
+    },
+  ],
+};
+export function PieChart() {
+  return (
+    <Box maxW='600px'>
+      <Pie data={data} />
+    </Box>
+  );
+}
