484e34ccdfb59cbbda099e1e693ce5c399ed588e
jasonLapina <lapina.jason@gmail.com>
2023-11-05 04:40:09 +0800

    reduce pie chart size

--- src/Admin/PieChart.js
+++ src/Admin/PieChart.js
@@ -29,7 +29,7 @@ export const data = {
 };
 export function PieChart() {
   return (
-    <Center w='600px' mx='auto'>
+    <Center w='360px' mx='auto'>
       <Pie data={data} />
     </Center>
   );
