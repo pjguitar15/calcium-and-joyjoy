e9d23088b78346f69ec8ffc32b86aa12867890f5
jasonLapina <lapina.jason@gmail.com>
2023-11-09 21:23:10 +0800

    init customize page

--- src/Customize/CustomizePage.js
+++ src/Customize/CustomizePage.js
@@ -1,6 +1,35 @@
-import { Box } from "@chakra-ui/react";
+import {
+  Box,
+  Button,
+  Circle,
+  Grid,
+  HStack,
+  Text,
+  VStack,
+} from "@chakra-ui/react";
 function CustomizePage() {
-  return <Box>customize mo to</Box>;
+  return (
+    <Box>
+      <HStack justifyContent='end' mb='24px' w='100%' gap='16px'>
+        <Button>Cancel</Button>
+        <Button>Done</Button>
+      </HStack>
+      <Grid justifyItems='center' gridTemplateColumns='1fr 1fr'>
+        <Box>image dito</Box>
+        <VStack>
+          <Text>Customize Your Shoes</Text>
+          <Box>
+            <Text>Lace Color:</Text>
+            <HStack gap='16px' mt='16px'>
+              <Circle aspectRatio='1/1' minW='24px' bgColor='black' />
+              <Circle aspectRatio='1/1' minW='24px' bgColor='red' />
+              <Circle aspectRatio='1/1' minW='24px' bgColor='blue' />
+            </HStack>
+          </Box>
+        </VStack>
+      </Grid>
+    </Box>
+  );
 }
 
 export default CustomizePage;
