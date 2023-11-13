2d1ae7cde012e347ebdd3be5c181d412ac962e53
jasonLapina <lapina.jason@gmail.com>
2023-11-14 01:42:13 +0800

    code cleanup, axios instance

--- src/Home/Homepage.js
+++ src/Home/Homepage.js
@@ -1,41 +1,10 @@
-import { Box, Grid, Skeleton, Text, VStack } from "@chakra-ui/react";
+import { VStack } from "@chakra-ui/react";
 import ShoeList from "./ShoeList";
 import Accessories from "./Accessories";
 import HeroCarousel from "./HeroCarousel";
 function Homepage() {
   return (
     <VStack align='normal' justify='normal' gap='80px'>
-      {/* <Grid
-        borderRadius='24px'
-        bgImage='linear-gradient(to bottom right, rgba(0,0,0,.9) ,rgba(0,0,0,.6))'
-        minH={{ base: "50vh", lg: "70vh" }}
-        pos='relative'
-        gridTemplateColumns={{ base: "1fr", lg: "1fr 1fr" }}
-        gridTemplateRows={{ base: "1fr 1fr", lg: "1fr" }}
-      >
-        <Text
-          fontWeight='bold'
-          filter='drop-shadow(3px 3px 3px #FF0707)'
-          fontSize={{ base: "10vw", lg: "120px" }}
-          color='white'
-          lineHeight='shorter'
-          pl={{ base: "24px", lg: "80px" }}
-          pt={{ base: "24px", lg: "80px" }}
-          letterSpacing='tight'
-        >
-          SAMPLE
-          <br />
-          PROTOTYPE
-        </Text>
-        <Box display='flex' pos='relative' alignItems='center'>
-          <Skeleton
-            w='100%'
-            h={{ base: "100%", lg: "400px" }}
-            pos='absolute'
-            // left={{ lg: "-3vw" }}
-          />
-        </Box>
-      </Grid> */}
       <HeroCarousel />
       <ShoeList />
       <Accessories />
---
+++ src/Shared/utils/axiosInstance.js
@@ -0,0 +1,5 @@
+import axios from "axios";
+
+const axiosInstance = axios.create({
+  baseURL: "https://adminserver-production.up.railway.app/api",
+});
