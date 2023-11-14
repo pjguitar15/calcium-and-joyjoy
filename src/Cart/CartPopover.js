86fa48d178a2875e44d8e42928d67d78c57b01e2
jasonLapina <lapina.jason@gmail.com>
2023-11-15 03:12:54 +0800

    cart popover handle no items

--- src/Cart/CartPopover.js
+++ src/Cart/CartPopover.js
@@ -41,61 +41,72 @@ const CartPopOver = () => {
       <PopoverContent w='400px' bgColor='#F2F0E6'>
         <PopoverArrow bgColor='#F2F0E6' />
         <PopoverCloseButton size='md' />
-        <PopoverBody mt='40px' px='32px' py='16px'>
-          {cart.map((item) => (
-            <Grid
-              maxH='320px'
-              gridTemplateColumns='1fr 1fr'
-              overflowY='auto'
-              justifyContent='center'
-              justifyItems='center'
-              columnGap='16px'
-            >
-              <Image src='/airJordan.png' />
-              <Box color='gray.500' fontSize='14px'>
-                <Text color='black' fontSize='16px' fontWeight='semibold'>
-                  Air Force 1 White
-                </Text>
-                <Text>Men/Women's Shoes</Text>
-                <HStack justifyContent='space-between'>
-                  <Text fontSize='12px'>Size: 8</Text>
-                  <Text fontSize='12px'>Quantity: 1</Text>
-                </HStack>
-                <HStack mt='-4px' justifyContent='space-between'>
-                  <Text>&#8369;5495</Text>
-                  <Button fontSize='14px' variant='unstyled' color='red.500'>
-                    Remove
-                  </Button>
-                </HStack>
-              </Box>
-            </Grid>
-          ))}
+        <PopoverBody
+          mt='40px'
+          px='32px'
+          pt='16px'
+          pb={cart.length < 1 ? "40px" : "16px"}
+        >
+          {cart.length < 1 ? (
+            <Text textAlign='center'>There are no items in your cart.</Text>
+          ) : (
+            cart.map((item) => (
+              <Grid
+                maxH='320px'
+                gridTemplateColumns='1fr 1fr'
+                overflowY='auto'
+                justifyContent='center'
+                justifyItems='center'
+                columnGap='16px'
+              >
+                <Image src='/airJordan.png' />
+                <Box color='gray.500' fontSize='14px'>
+                  <Text color='black' fontSize='16px' fontWeight='semibold'>
+                    Air Force 1 White
+                  </Text>
+                  <Text>Men/Women's Shoes</Text>
+                  <HStack justifyContent='space-between'>
+                    <Text fontSize='12px'>Size: 8</Text>
+                    <Text fontSize='12px'>Quantity: 1</Text>
+                  </HStack>
+                  <HStack mt='-4px' justifyContent='space-between'>
+                    <Text>&#8369;5495</Text>
+                    <Button fontSize='14px' variant='unstyled' color='red.500'>
+                      Remove
+                    </Button>
+                  </HStack>
+                </Box>
+              </Grid>
+            ))
+          )}
           {/* GO TO CART/CHECKOUT */}
-          <HStack mt='40px' justifyContent='center' align='normal'>
-            <Link to='/cart'>
-              <Button
-                variant='outline'
-                fontSize='15px'
-                px='32px'
-                borderRadius='20px'
-                w='100%'
-                border='solid 1px red'
-              >
-                View Cart
-              </Button>
-            </Link>
-            <Link to='/checkout'>
-              <Button
-                bgColor='red'
-                fontSize='15px'
-                px='32px'
-                borderRadius='20px'
-                w='100%'
-              >
-                Checkout
-              </Button>
-            </Link>
-          </HStack>
+          {cart.length > 1 && (
+            <HStack mt='40px' justifyContent='center' align='normal'>
+              <Link to='/cart'>
+                <Button
+                  variant='outline'
+                  fontSize='15px'
+                  px='32px'
+                  borderRadius='20px'
+                  w='100%'
+                  border='solid 1px red'
+                >
+                  View Cart
+                </Button>
+              </Link>
+              <Link to='/checkout'>
+                <Button
+                  bgColor='red'
+                  fontSize='15px'
+                  px='32px'
+                  borderRadius='20px'
+                  w='100%'
+                >
+                  Checkout
+                </Button>
+              </Link>
+            </HStack>
+          )}
         </PopoverBody>
       </PopoverContent>
     </Popover>
