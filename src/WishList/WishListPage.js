8e085b141bb8aa38fdf4bcc20bb96054eec044cc
jasonLapina <lapina.jason@gmail.com>
2023-11-26 07:16:05 +0800

    add auth prompt on wishlist page

--- src/WishList/WishListPage.js
+++ src/WishList/WishListPage.js
@@ -1,12 +1,24 @@
-import { Box, Heading, Text } from "@chakra-ui/react";
+import { Box, Button, Heading, Text } from "@chakra-ui/react";
+import { Link } from "react-router-dom";
 function WishListPage() {
+  const user = localStorage.getItem("user");
+
   return (
     <Box>
       <Box textAlign='center'>
         <Heading mb='16px' fontWeight='semibold'>
           Wishlist
         </Heading>
-        <Text>There are no items in your wishlist.</Text>
+        {user ? (
+          <Text>There are no items in your wishlist.</Text>
+        ) : (
+          <Box>
+            <Text mb='16px'>You need an account to have a wishlist.</Text>
+            <Link to='/auth/sign-in'>
+              <Button variant='link'>Sign in</Button>
+            </Link>
+          </Box>
+        )}
       </Box>
     </Box>
   );
