b435dba5d83bb14e214cf6ea9352081300f865ab
jasonLapina <lapina.jason@gmail.com>
2023-12-06 20:04:38 +0800

    remove joypop

--- src/Shared/UI/Layout.js
+++ src/Shared/UI/Layout.js
@@ -5,7 +5,7 @@ import Navbar from "./Navbar";
 import { useEffect } from "react";
 import DrawerNav from "./DrawerNav";
 import Footer from "./Footer";
-import Joypop from "../../Joybot/Joypop";
+// import Joypop from "../../Joybot/Joypop";
 function Layout() {
   const [isLg] = useMediaQuery("(min-width: 992px)");
   const { pathname } = useLocation();
@@ -19,7 +19,7 @@ function Layout() {
       {!isLg && <DrawerNav />}
 
       {/* <Box display='flex' flexDir='column' minH='100vh'> */}
-      <Joypop />
+      {/* <Joypop /> */}
       <Box
         flexGrow='1'
         maxW='var(--maxW)'
