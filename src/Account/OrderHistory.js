d618fb9fea3f146b4d03fa3d595a3997df24a530
jasonLapina <lapina.jason@gmail.com>
2023-12-08 17:51:17 +0800

    get orders from db

--- src/Account/OrderHistory.js
+++ src/Account/OrderHistory.js
@@ -1,6 +1,21 @@
 import { Box } from "@chakra-ui/react";
+import { useQuery } from "react-query";
+import LoadingSpinner from "../Shared/UI/LoadingSpinner";
+import axiosInstance from "../Shared/utils/axiosInstance";
 function OrderHistory() {
-  return <Box>No order history.</Box>;
+  const user = JSON.parse(localStorage.getItem("user"));
+
+  const getOrders = async () => {
+    const res = await axiosInstance.get("/user/order", {
+      headers: { Authorization: `Bearer ${user?.token}` },
+    });
+    return res.data;
+  };
+  const { data: orders, isLoading } = useQuery("orders", getOrders);
+  if (isLoading) return <LoadingSpinner />;
+  if (orders.length < 1) return <Box>No order history.</Box>;
+
+  return <Box>List of orders here</Box>;
 }
 
 export default OrderHistory;
