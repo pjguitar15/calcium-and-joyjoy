628e393ac831cc1742c111b41157a3ff9e86d37a
philcob suzuki <philcobsuzuki@gmail.com>
2023-12-15 01:57:49 +0800

    fixed order details page, line chart now dynamic

--- src/Product/ProductList.js
+++ src/Product/ProductList.js
@@ -3,7 +3,6 @@ import ItemCard from "../Shared/UI/ItemCard";
 import LoadingSpinner from "../Shared/UI/LoadingSpinner";
 import Scrollbars from "rc-scrollbars";
 import { useSearchParams } from "react-router-dom";
-import { useGetAllProducts } from '../Shared/Hooks/useGetAllProducts';
 import { useGetShoes } from '../Shared/Hooks/useShoes';
 
 function ProductList() {
@@ -14,14 +13,8 @@ function ProductList() {
     queryObj[key] = value;
   }
 
-  const keyword = queryObj.keyword;
-
   const { data, isLoading } = useGetShoes();
   if (isLoading) return <LoadingSpinner />;
-
-  const filteredData = keyword
-    ? data.filter(item => item.name.toLowerCase().includes(keyword.toLowerCase()))
-    : data;
 
   return (
     <Box as={Scrollbars}>
@@ -31,7 +24,7 @@ function ProductList() {
         gridTemplateColumns='repeat(auto-fit,minmax(320px,1fr))'
         py='16px'
       >
-        {data.map((item) => (
+        {data?.map((item) => (
           <ItemCard data={item} key={item.id} cardW='300px' />
         ))}
       </Grid>
