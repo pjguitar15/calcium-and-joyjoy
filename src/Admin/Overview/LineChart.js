cc34a92a803f254235108c29f5262e346fbf07d9
Ace Brian <ace.brian2409@gmail.com>
2023-12-17 10:52:00 +0800

    fix dashboard overview designs

--- src/Admin/Overview/LineChart.js
+++ src/Admin/Overview/LineChart.js
@@ -1,4 +1,6 @@
-import React, { useEffect, useState } from "react"
+import React, { useEffect, useState } from "react";
+import { Line } from "react-chartjs-2";
+import axios from 'axios';
 import {
   Chart as ChartJS,
   CategoryScale,
@@ -8,9 +10,7 @@ import {
   Title,
   Tooltip,
   Legend,
-} from "chart.js"
-import { Line } from "react-chartjs-2"
-import axios from 'axios'
+} from "chart.js";
 
 ChartJS.register(
   CategoryScale,
@@ -20,106 +20,85 @@ ChartJS.register(
   Title,
   Tooltip,
   Legend
-)
+);
 
 const options = {
   responsive: true,
   scales: {
     x: {
-      ticks: {
-        color: "white", // Set the color of x-axis labels to white
-      },
+      ticks: { color: "white" },
     },
     y: {
-      ticks: {
-        color: "white", // Set the color of y-axis labels to white
-      },
+      ticks: { color: "white" },
     },
   },
   plugins: {
     legend: {
       position: "top",
-      labels: {
-        color: "white", // Set the color of legend labels to white
-      },
+      labels: { color: "white" },
     },
     title: {
       display: true,
       text: "Sales",
-      color: "white", // Set the color of the chart title to white
+      color: "white",
     },
   },
-}
+};
 
 const LineChart = () => {
+  const [chartData, setChartData] = useState({ labels: [], data: [] });
+  const [isLoading, setIsLoading] = useState(true);
+  const [error, setError] = useState(null);
 
+  useEffect(() => {
+    axios.get("http://18.223.157.202/backend/api/admin/sales")
+      .then((response) => {
+        const activeCouriers = response.data.data;
+        const modifiedData = activeCouriers.datasets[0].data.map(item =>
+          parseFloat(item.replace(/,/g, '')).toFixed(2)
+        );
 
- 
-  
-  const [labels, setLabels] = useState([]);
-  const [dataFromDataSets, setDataFromDataSets] = useState([]);
-  
-  useEffect(() => {
-      const fetchCouriersAndPaymentMethods = async () => {
-          try {
-              const [couriersResponse] = await Promise.all([
-                  axios.get("http://18.223.157.202/backend/api/admin/sales"),
-              ]);
-  
-              const activeCouriers = couriersResponse.data.data;
-              const modifyDataSetsData = activeCouriers.datasets[0].data.map((item) => {
-                  return parseFloat(item.replace(/,/g, '')).toFixed(2);
-              });
-  
-              setLabels(activeCouriers.labels);
-              setDataFromDataSets(modifyDataSetsData);
-          } catch (error) {
-              // Handle error
-              console.error(error);
-          }
-      };
-  
-      fetchCouriersAndPaymentMethods();
-  }, []); // Empty dependency array to run the effect only once
-  
-  useEffect(() => {
-      console.log('hakdog', dataFromDataSets);
-  }, [dataFromDataSets]);
-  
+        setChartData({
+          labels: activeCouriers.labels,
+          data: modifiedData,
+        });
+      })
+      .catch(error => {
+        setError(error);
+      })
+      .finally(() => {
+        setIsLoading(false);
+      });
+  }, []);
+
+  if (isLoading) {
+    return <p>Loading...</p>;
+  }
+
+  if (error) {
+    return <p>Error loading data</p>;
+  }
 
   const data = {
-    // x axis (array)
-    labels,
+    labels: chartData.labels,
     datasets: [
       {
         label: "Sales",
-        // y axis
-        data: dataFromDataSets, // (array)
+        data: chartData.data,
         borderColor: "white",
-        backgroundColor: "white",
+        backgroundColor: "rgba(255, 255, 255, 0.5)",
       },
     ],
-  }
-
-
-
+  };
 
   return (
     <div className="p-5 bg-gray-100 border border-gray-400 rounded-md">
-      
       <div className="bg-green-700 p-6">
-        {dataFromDataSets ? (
-          data ? (
-            <Line options={options} data={data} />
-          ) : (
-            <p>Loading...</p> // or any loading state or placeholder you prefer
-          )
-        ) : null}
+        <Line options={options} data={data} />
       </div>
       <h1 className="text-2xl mt-4 font-semibold">Product Sales</h1>
-     
     </div>
-  )
-}
+  );
+};
 
-export default LineChart
+export default LineChart;
