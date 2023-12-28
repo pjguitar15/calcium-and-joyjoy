cbf0d22d68f72741c34c1a02cc77d9d671f8b588
Ace Brian <ace.brian2409@gmail.com>
2023-12-29 02:22:27 +0800

    changes in account details

--- src/Account/OrderDetails/OrderDetails.js
+++ src/Account/OrderDetails/OrderDetails.js
@@ -1,56 +1,77 @@
-import React, { useEffect, useState } from "react";
-import axios from "axios";
+import React, { useEffect, useState, useCallback, useMemo } from "react";
+import axiosInstance from "../../Shared/utils/axiosInstance";
 import OrderDetailItem from "./OrderDetailItem";
+import { Box, Heading, Text, CircularProgress, Center } from "@chakra-ui/react";
 
 function OrderDetails() {
     const [currUserOrders, setCurrUserOrders] = useState([]);
-    const user = localStorage.getItem("user");
-    const parsedUser = JSON.parse(user);
+    const [isLoading, setIsLoading] = useState(true);
+    const [error, setError] = useState('');
+
+    const user = JSON.parse(localStorage.getItem("user"));
+
+    const fetchOrders = useCallback(async () => {
+        try {
+            const response = await axiosInstance.get("/user/orders", {
+                headers: {
+                    Authorization: `Bearer ${user?.token}`,
+                },
+            });
+            return response.data.data?.filter(
+                (item) => item.items.length > 0 && item.status !== 'RECEIVED'
+            );
+        } catch (err) {
+            setError('Error fetching orders. Please try again later.');
+            console.error("Error fetching orders:", err);
+        }
+    }, [user?.token]);
 
     useEffect(() => {
-        axios
-            .get(`http://18.223.157.202/backend/api/user/orders`, {
-                headers: {
-                    Authorization: `Bearer ${parsedUser?.token}`,
-                },
-            })
-            .then((res) => {
-                // Filter out orders with status 'RECEIVED' right after fetching
-                const filteredData = res.data.data?.filter(
-                    (item) => item.items.length > 0 && item.status !== 'RECEIVED'
-                );
+        fetchOrders().then(filteredData => {
+            if (filteredData) {
                 setCurrUserOrders(filteredData);
-            });
-    }, []);
+            }
+            setIsLoading(false);
+        });
+    }, [fetchOrders]);
 
-    const handleMarkAsReceived = (order) => {
-        axios
-            .post(`http://18.223.157.202/backend/api/admin/orders/${order.id}/status`, {
-                status: 'RECEIVED',
-            }, {
-                headers: {
-                    Authorization: `Bearer ${parsedUser?.token}`,
-                },
-            })
-            .then(response => {
-                // Remove the order from the state after marking it as received
-                const remainingOrders = currUserOrders.filter(item => item.id !== order.id);
-                setCurrUserOrders(remainingOrders);
-            })
-            .catch(error => {
-                console.error("Error updating order status:", error);
-            });
-    };
+    const handleMarkAsReceived = useCallback((order) => {
+        axiosInstance.post(`/admin/orders/${order.id}/status`, {
+            status: 'RECEIVED',
+        }, {
+            headers: {
+                Authorization: `Bearer ${user?.token}`,
+            },
+        })
+        .then(response => {
+            const remainingOrders = currUserOrders.filter(item => item.id !== order.id);
+            setCurrUserOrders(remainingOrders);
+        })
+        .catch(error => {
+            console.error("Error updating order status:", error);
+        });
+    }, [currUserOrders, user?.token]);
+
+    const renderedOrders = useMemo(() => currUserOrders.map((item, index) => (
+        <OrderDetailItem 
+            key={index} 
+            orderItems={item} 
+            onReceived={() => handleMarkAsReceived(item)} 
+        />
+    )), [currUserOrders, handleMarkAsReceived]);
 
     return (
         <main className='py-8 pe-8 overflow-y-scroll max-h-[800px] flex flex-col gap-3'>
-            {currUserOrders?.map((item, index) => (
-                <OrderDetailItem 
-                    key={index} 
-                    orderItems={item} 
-                    onReceived={() => handleMarkAsReceived(item)} 
-                />
-            ))}
+            <Heading as="h3" size="lg" mb="6">My Orders</Heading>
+            {isLoading ? (
+                <Center><CircularProgress isIndeterminate color="green.300" /></Center>
+            ) : error ? (
+                <Text color="red.500">{error}</Text>
+            ) : currUserOrders?.length > 0 ? (
+                renderedOrders
+            ) : (
+                <Text>No orders to display. Start shopping now!</Text>
+            )}
         </main>
     );
 }
