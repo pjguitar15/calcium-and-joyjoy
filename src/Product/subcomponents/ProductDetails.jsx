d58be328dc77befd48f8b3dea0d00352da803c6e
Ace Brian <ace.brian2409@gmail.com>
2023-12-30 02:52:26 +0800

    fixed some bugs in itemcard, will continue later

--- src/Product/subcomponents/ProductDetails.jsx
+++ src/Product/subcomponents/ProductDetails.jsx
@@ -7,18 +7,24 @@ const ProductDetails = ({ shoe, onAddToCart, onWishList, onCheckout, selectedSiz
         setQty(value);
     };
 
-     // Check if a size is available in shoe
-     const isSizeAvailable = (size) => {
-        return shoe.sizes.some(s => s.size.name === size);
+    // Check if a size is available in shoe
+    const isSizeAvailable = (size) => {
+        return shoe.sizes && shoe.sizes.some(s => s.size.name === size);
     };
+
+    // Check if the product has sizes
+    const hasSizes = shoe.sizes && shoe.sizes.length > 0;
+
+    // Determine the product type text
+    const productTypeText = shoe.category ? shoe.category.name : (shoe.gender === "male" ? "Men's" : "Women's") + ' shoes';
 
     return (
         <Box padding="4" borderWidth="1px" borderRadius="lg" overflow="hidden">
-            <Heading as="h2" size="xl" fontWeight="bold" mb="4">{shoe.name}</Heading>
-            <Text fontSize="lg" color="gray.600" mb="2">{shoe.gender === "male" ? "Men's" : "Women's"} shoes</Text>
-            <Text fontSize="2xl" fontWeight="bold" my="2">{convertCurrency(shoe.price)}</Text>
-            <Text my="2" mb="4">Colors: {shoe.colors.map(colorObj => colorObj.color.name).join(', ')}</Text>
-            <HStack my="4">
+        <Heading as="h2" size="xl" fontWeight="bold" mb="4">{shoe.name}</Heading>
+        <Text fontSize="lg" color="gray.600" mb="2">{productTypeText}</Text>
+        <Text fontSize="2xl" fontWeight="bold" my="2">{convertCurrency(shoe.price)}</Text>
+        <Text my="2" mb="4">Colors: {shoe.colors.map(colorObj => colorObj.color.name).join(', ')}</Text>
+        <HStack my="4">
                 <Text fontWeight="bold">Quantity:</Text>
                 <InputGroup size="sm" maxW="140px">
                     <InputLeftAddon children='-' cursor="pointer" onClick={() => setQty(qty > 1 ? qty - 1 : 1)} />
@@ -26,23 +32,29 @@ const ProductDetails = ({ shoe, onAddToCart, onWishList, onCheckout, selectedSiz
                     <InputRightAddon children='+' cursor="pointer" onClick={() => setQty(qty < 10 ? qty + 1 : 10)} />
                 </InputGroup>
             </HStack>
-                       <Text fontWeight="bold" mb="2">Sizes</Text>
-            <Grid templateColumns="repeat(4, 1fr)" gap="2" mb="4">
-                {sizes.map((size) => (
-                    <Button
-                        key={size}
-                        variant={selectedSize === size ? 'solid' : 'outline'}
-                        borderColor={selectedSize === size ? "#daa520" : "gray.300"}
-                        borderWidth={selectedSize === size ? "2px" : "1px"}
-                        color={selectedSize === size ? "#daa520" : "gray.800"}
-                        _hover={{ bg: selectedSize === size ? "#fff5c9" : "gray.100" }}
-                        onClick={() => setSelectedSize(size)}
-                        isDisabled={!isSizeAvailable(size)} // Disable the button if size is not available
-                    >
-                        US {size}
-                    </Button>
-                ))}
-            </Grid>
+
+            {hasSizes && (
+                <>
+                    <Text fontWeight="bold" mb="2">Sizes</Text>
+                    <Grid templateColumns="repeat(4, 1fr)" gap="2" mb="4">
+                        {sizes.map((size) => (
+                            <Button
+                                key={size}
+                                variant={selectedSize === size ? 'solid' : 'outline'}
+                                borderColor={selectedSize === size ? "#daa520" : "gray.300"}
+                                borderWidth={selectedSize === size ? "2px" : "1px"}
+                                color={selectedSize === size ? "#daa520" : "gray.800"}
+                                _hover={{ bg: selectedSize === size ? "#fff5c9" : "gray.100" }}
+                                onClick={() => setSelectedSize(size)}
+                                isDisabled={!isSizeAvailable(size)}
+                            >
+                                US {size}
+                            </Button>
+                        ))}
+                    </Grid>
+                </>
+            )}
+
             <VStack spacing="4">
                 <HStack spacing="4" w="full">
                     <Button onClick={onAddToCart} bgColor='gray' color='white' borderRadius='20px' w="full">Add to Cart</Button>
--- src/Shared/UI/ItemCard.js
+++ src/Shared/UI/ItemCard.js
@@ -1,53 +1,40 @@
 import { FaRegHeart, FaCartPlus } from 'react-icons/fa';
 import {
-  Box,
-  Card,
-  CardBody,
-  CardHeader,
-  HStack,
-  Image,
-  Text,
-  IconButton,
-  useToast,
+  Box, Card, CardBody, CardHeader, HStack, Image, Text, IconButton, useToast,
+  LinkBox, LinkOverlay,
 } from "@chakra-ui/react";
-import { Link, useLocation } from "react-router-dom"
-import LoadingSpinner from "./LoadingSpinner"
-import convertCurrency from "../utils/convertCurrency"
+import { Link, useLocation } from "react-router-dom";
+import LoadingSpinner from "./LoadingSpinner";
+import convertCurrency from "../utils/convertCurrency";
 import { useDispatch } from "react-redux";
 import { addToCart } from "../../Store/cart";
 import axiosInstance from "../utils/axiosInstance";
 
-
-function ItemCard(props) {
+function ItemCard({ cardW, data, onSelect, isSelected }) {
   const user = JSON.parse(localStorage.getItem("user"));
   const toast = useToast();
   const dispatch = useDispatch();
-  const { cardW, data, onSelect, isSelected } = props
+  const { pathname } = useLocation();
 
-  const { pathname } = useLocation()
+  if (!data) return <LoadingSpinner />;
 
-  if (!data) return <LoadingSpinner />
-
-  const { name, price, gender, discount, id } = data
-  const maxLength = 21
-  const formattedName =
-    name.trim().length > maxLength ? name.slice(0, maxLength) + "..." : name
+  const { name, price, gender, discount, id, category, image } = data;
+  const maxLength = 21;
+  const formattedName = name.length > maxLength ? `${name.slice(0, maxLength)}...` : name;
 
   const clickHandler = () => {
-    if (pathname !== "/customize") return
-    onSelect(data)
-  }
+    if (pathname === "/customize") {
+      onSelect(data);
+    }
+  };
 
   const handleWishlist = async (e) => {
     e.stopPropagation();
     if (!user) {
-      toast({
-        position: "top",
-        status: "error",
-        description: "Please login to add items to your wishlist.",
-      });
+      toast({ position: "top", status: "error", description: "Please login to add items to your wishlist." });
       return;
     }
+
     try {
       await axiosInstance.post("/user/wishlist/store", {
         user_id: user.user_info.id,
@@ -55,113 +42,46 @@ function ItemCard(props) {
       }, { headers: { Authorization: `Bearer ${user.token}` } });
       toast({ position: "top", status: "success", title: "Added to wishlist" });
     } catch (error) {
-      toast({
-        position: "top",
-        status: "error",
-        description: "An error occurred while adding to wishlist.",
-      });
+      toast({ position: "top", status: "error", description: "An error occurred while adding to wishlist." });
     }
   };
 
   const handleAddToCart = (e) => {
     e.stopPropagation();
-    dispatch(addToCart({
-      ...data,
-      quantity: 1,
-    }));
-    toast({
-      position: "top",
-      status: "success",
-      title: "Added to cart",
-    });
+    dispatch(addToCart({ ...data, quantity: 1 }));
+    toast({ position: "top", status: "success", title: "Added to cart" });
   };
 
   return (
-    <Card
-      onClick={clickHandler}
-      py="8px"
-      boxShadow="4px 4px 16px rgba(0,0,0,.3)"
-      w={cardW || "250px"}
-      mx="auto"
-      cursor="pointer"
-      transition="all .4s"
-      _hover={{
-        transform: "translateY(-8px)",
-        boxShadow: "4px 8px 16px rgba(0,0,0,.4)",
-      }}
-      as={pathname !== "/customize" ? Link : Card}
-      to={`/shoe/${id}`}
-      borderRadius="20px"
-      h="400px"
-      bgColor={isSelected ? "var(--accent)" : ""}
-    >
+    <LinkBox as={Card} py="8px" boxShadow="4px 4px 16px rgba(0,0,0,.3)" w={cardW || "250px"} mx="auto" cursor="pointer" transition="all .4s" _hover={{ transform: "translateY(-8px)", boxShadow: "4px 8px 16px rgba(0,0,0,.4)" }} borderRadius="20px" h="400px" bgColor={isSelected ? "var(--accent)" : ""} onClick={clickHandler}>
       {discount && (
         <CardHeader color="white" pos="relative" mt="-8px">
-          <Box
-            w="fit-content"
-            pos="absolute"
-            right="24px"
-            top="0"
-            bgColor="#F24E1E"
-            px="4px"
-            py="12px"
-            clipPath="polygon(100% 0, 100% 100%, 50% 80%, 0 100%, 0 0)"
-            fontSize="14px"
-          >
-            {discount * 100}%
-          </Box>
+          <Box w="fit-content" pos="absolute" right="24px" top="0" bgColor="#F24E1E" px="4px" py="12px" clipPath="polygon(100% 0, 100% 100%, 50% 80%, 0 100%, 0 0)" fontSize="14px">{Math.round(discount * 100)}%</Box>
         </CardHeader>
       )}
       <CardBody>
-        <Box display="flex" alignItems="center" alignContent="center" h="200px">
-          <Image
-            alt="product"
-            mx="auto"
-            src={props.data.image}
-            verticalAlign="bottom"
-            mb="16px"
-            maxH="200px"
-            // h='200px'
-            borderRadius="10px"
-          />
-        </Box>
+      <Box display="flex" justifyContent="center" alignItems="center" h="200px">
+          <LinkOverlay as={Link} to={pathname !== "/customize" ? `/shoe/${id}` : undefined}>
+            <Image alt="product" mx="auto" src={image} verticalAlign="bottom" mb="16px" maxH="200px" borderRadius="10px" />
+          </LinkOverlay>
+      </Box>
         <Text fontWeight="semibold">{formattedName}</Text>
-
-        <Text color="gray.500">
-          {gender === "male" ? "Men's" : "Women's"}{" "}
-          {props.data.category && props.data.category?.name}
-        </Text>
-
+        <Text color="gray.500">{category?.name || ''} - {gender === "male" ? "Men's" : "Women's"}</Text>
         {discount ? (
           <HStack>
-            <Text color="red" fontWeight="semibold">
-            {convertCurrency(price * (1 - discount))}
-            </Text>
-            <Text my="16px" textDecor="line-through" fontWeight="semibold">
-              {convertCurrency(price)}
-            </Text>
+            <Text color="red" fontWeight="semibold">{convertCurrency(price * (1 - discount))}</Text>
+            <Text my="16px" textDecor="line-through" fontWeight="semibold">{convertCurrency(price)}</Text>
           </HStack>
         ) : (
-          <Text my="16px" fontWeight="semibold">
-            {convertCurrency(price)}
-          </Text>
+          <Text my="16px" fontWeight="semibold">{convertCurrency(price)}</Text>
         )}
-
-<HStack spacing={4} mt={2}>
-        <IconButton
-            aria-label="Add to cart"
-            icon={<FaCartPlus />}
-            onClick={handleAddToCart}
-          />
-          <IconButton
-            aria-label="Add to wishlist"
-            icon={<FaRegHeart />}
-            onClick={handleWishlist}
-          />
+        <HStack spacing={4} mt={2}>
+          <IconButton aria-label="Add to cart" icon={<FaCartPlus />} onClick={handleAddToCart} />
+          <IconButton aria-label="Add to wishlist" icon={<FaRegHeart />} onClick={handleWishlist} />
         </HStack>
       </CardBody>
-    </Card>
-  )
+    </LinkBox>
+  );
 }
 
-export default ItemCard
+export default ItemCard;
